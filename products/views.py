a49261be38bcaf734190b3397711fa174e0024a3
Markus Marko <romanko0815@gmail.com>
2016-03-17 15:48:46 +0200

    add very simple search for products; add input for search to navbar;

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from products.models import Product
@@ -14,3 +15,18 @@ class ProductDetailView(DetailView):
 
 class ProductListView(ListView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
+
+    def get_queryset(self):
+        qs = super(ProductListView, self).get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            qs = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return qs
