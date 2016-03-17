ce15af6c00ae519495cf52c45c7d245813b86f6c
Markus Marko <romanko0815@gmail.com>
2016-03-17 18:21:35 +0200

    create formset for Inventory

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from products.views import ProductDetailView, ProductListView
+from products.views import ProductDetailView, ProductListView, VariationListView
 
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='product_list'),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, ListView
-from products.models import Product
+
+from products.forms import VariationInventoryFormSet
+from products.models import Product, Variation
 
 
 class ProductDetailView(DetailView):
@@ -30,3 +33,38 @@ class ProductListView(ListView):
                 Q(description__icontains=query)
             )
         return qs
+
+
+class VariationListView(ListView):
+    model = Variation
+    queryset = Variation.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(VariationListView, self).get_context_data(**kwargs)
+        context['formset'] = VariationInventoryFormSet(queryset=self.get_queryset())
+        context['product'] = get_object_or_404(Product, pk=self.kwargs.get('pk'))
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        product_pk = self.kwargs.get('pk')
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            qs = Variation.objects.filter(product=product)
+        return qs
+
+    def post(self, request, *args, **kwargs):
+        formset = VariationInventoryFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            print 'valid'
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                product_pk = self.kwargs.get('pk')
+                product = get_object_or_404(Product, pk=product_pk)
+                new_item.product = product
+                new_item.save()
+            messages.success(request, 'Your inventory and pricing has been updated')
+            return redirect('products:product_list')
+        print 'invalid'
+
+
